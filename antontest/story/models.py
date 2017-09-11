12fca033debf2bf720654668c4e6bbda6d179330
mt-christo <slepnev@yandex.ru>
2017-09-11 16:57:06 +0300

    Update models.py

--- antontest/story/models.py
+++ antontest/story/models.py
@@ -3,16 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    name = models.CharField(max_length=255)
-    imgpath = models.CharField(max_length=255)
-    descshort = models.CharField(max_length=2000)
-    part = models.CharField(max_length=2000)
-    coupon = models.CharField(max_length=2000)
-    desclong = models.CharField(max_length=2000)
+    name = models.CharField(max_length=255, default='[NO NAME]')
+    imgpath = models.CharField(max_length=255, default='')
+    descshort = models.CharField(max_length=2000, default='')
+    part = models.CharField(max_length=2000, default='')
+    coupon = models.CharField(max_length=2000, default='')
+    desclong = models.CharField(max_length=2000, default='')
     provider = models.ManyToManyField('Provider')
 
 class Provider(models.Model):
-    name = models.CharField(max_length=255)
-    imgpath = models.CharField(max_length=255)
-    desc = models.CharField(max_length=2000)
+    name = models.CharField(max_length=255, default='')
+    imgpath = models.CharField(max_length=255, default='')
+    desc = models.CharField(max_length=2000, default='')
     
