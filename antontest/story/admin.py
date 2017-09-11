674c2b398721db80114764517be3d6163344ae45
mt-christo <slepnev@yandex.ru>
2017-09-11 17:58:29 +0300

    Update admin.py

--- antontest/story/admin.py
+++ antontest/story/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from .models import Product, Provider
 
 @admin.register(Product)
-class pa(admin.Modeladmin):  
+class pa(admin.ModelAdmin):  
   list_display = ('name', 'imgpath', 'descshort', 'part', 'coupon', 'desclong', 'provider')
 
 @admin.register(Provider)
-class pr(admin.Modeladmin):  
+class pr(admin.ModelAdmin):  
   list_display = ('name', 'imgpath', 'desc')
   
 # Register your models here.
