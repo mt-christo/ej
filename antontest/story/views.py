e1afe66b8b129a07c6340e1b22e79a2378d5a1a7
mt-christo <slepnev@yandex.ru>
2017-09-23 11:11:44 +0300

    Update views.py

--- antontest/story/views.py
+++ antontest/story/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from .models import Product, Provider
+import numpy as np
+import random
 
 # Create your views here.
 
@@ -10,5 +12,9 @@ def allproducts(request):
     return render_to_response("story/allproducts.html", {'products' : Product.objects.all()})
 
 def product(request,id):
-    return render_to_response("story/product.html", {'product' : Product.objects.get(pk=id), 'data':[[1,2],[2,1],[3,4],[6,2]]})
+    n = np.array(range(1000)) + 1
+    d = np.cumsum(np.array([random.random() for x in n]) - 0.5)
+    data = [[i,d[i]] for i in n]
+    product = Product.objects.get(pk=id)
+    return render_to_response("story/product.html", {'product' : product, 'data':data})
 
