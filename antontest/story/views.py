a7098a5e5e3869eb34942385f35eeeb040ee17fc
mt-christo <slepnev@yandex.ru>
2017-09-23 10:56:57 +0300

    Update views.py

--- antontest/story/views.py
+++ antontest/story/views.py
@@ -10,5 +10,5 @@ def allproducts(request):
     return render_to_response("story/allproducts.html", {'products' : Product.objects.all()})
 
 def product(request,id):
-    return render_to_response("story/product.html", {'product' : Product.objects.get(pk=id)})
+    return render_to_response("story/product.html", {'product' : Product.objects.get(pk=id), 'data':[[1,2],[2,1],[3,4],[6,2]]})
 
