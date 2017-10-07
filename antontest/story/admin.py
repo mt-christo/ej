9a37823b1faf1486f20c84b94a75241ba0133fcc
mt-christo <slepnev@yandex.ru>
2017-10-07 12:50:46 +0300

    Update admin.py

--- antontest/story/admin.py
+++ antontest/story/admin.py
@@ -3,7 +3,7 @@ from .models import Product, Provider
 
 @admin.register(Product)
 class pa(admin.ModelAdmin):  
-  list_display = ('name', 'descshort', 'logopath', 'explainpath', 'explainpath2', 'tspath', 'templatepath', 'participation', 'coupon', 'listProviders')
+  list_display = ('name', 'titlethumb', 'titlecard', 'logopath', 'explainpath', 'explainpath2', 'tspath', 'templatepath', 'participation', 'coupon', 'listProviders')
 
 @admin.register(Provider)
 class pr(admin.ModelAdmin):  
