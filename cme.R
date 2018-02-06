b6d25a2ebb779cfe26f8fe228199cd10808bbe23
Anton <slepnev@yandex.ru>
2018-02-06 12:46:54 +0300

    aaa

--- cme.R
+++ cme.R
@@ -6,21 +6,7 @@ library(lubridate)
 
 MONTH_CODES = data.frame(code = c('F','G','H','J','K','M','N','Q','U','V','X','Z'), n = 1:12, stringsAsFactors=FALSE)
 CALENDARS = list()
-for(x in names(r)) CALENDARS[[x]] = which(foreach(y = r[[x]],.combine=c)%do%(length(y)>1))
-CALENDARS[['FC']] = c(1,3,4,5,8,9,10,11)
-CALENDARS[['LC']] = c(2,4,6,8,10,12)
-CALENDARS[['LH']] = c(2,4,5,6,7,8,10,12)
-CALENDARS[['CL']] = c(1,2,3,4,5,6,7,8,9,10,11,12)
-CALENDARS[['RB']] = c(1,2,3,4,5,6,7,8,9,10,11,12)
-CALENDARS[['PB']] = c(2,4,5,6,7,8,10,12)
-CALENDARS[['C']] = c(3,5,7,9,12)
-CALENDARS[['W']] = c(3,5,7,9,12)
-CALENDARS[['S']] = c(1,3,5,7,8,9,11)
-CALENDARS[['SB']] = c(1,3,5,7,8,9,10,12)
-CALENDARS[['LB']] = c(1,3,5,7,9,11)
-CALENDARS[['CC']] = c(3,5,7,9,12)
-CALENDARS[['CT']] = c(3,5,7,10,12)
-CALENDARS[['OJ']] = c(1,3,5,7,9,11)
+for(x in names(r)) CALENDARS[[x]] = which(foreach(y = r[[x]],.combine=c)%do%(length(y)>1 & max(index(y))>'2000-01-01' & length(y[index(y)>'2000-01-01'])>1000))
 
 #max_date = as.Date('1900-01-01')
 #for(d in c('~/FUT/RB','~/FUT/CL'))
@@ -87,12 +73,17 @@ for(n in as.vector(unlist(fread('~/FUT/Lots.txt', fill=TRUE)[,1]))){
 }
 
 save(r, file="CME-20170120.RData")
+r = get(load(file="CME-20170120.RData"))
+
 }
 
 # name='FC'; months=c('Q','U','V'); weights=c(1,-2,1); oi_min=100
-# name='LH'; months=c(4,5,6); weights=c(1,-2,1); oi_min=10; dt_start='2008-01-01'; dt_end='2012-01-01'
+# name='LC'; months=c(7,8,9); weights=c(1,-2,1); oi_min=10; dt_start='2008-01-01'; dt_end='2012-01-01'
 mmspread = function(name, months, weights, oi_min, dt_start='2011-01-01', dt_end='2018-01-01'){
     month_letters = if(months[1]%in%MONTH_CODES$code) months else MONTH_CODES$code[months]
+    month_ns = MONTH_CODES$n[match(month_letters, MONTH_CODES$code)]
+    m1 = head(month_ns,1); mn = tail(month_ns,1)
+    months_exclude = if(m1 < mn) m1:mn else c(1:mn,m1:12)
     res = foreach(i = 1:length(month_letters),.combine='+')%do%{
         x = r[[name]][[month_letters[i]]]
         names(x) = c('Open','High','Low','Close','Volume','OpenInt')
@@ -102,31 +93,82 @@ mmspread = function(name, months, weights, oi_min, dt_start='2011-01-01', dt_end
         names(x) = c('Open','High','Low','Close','Volume','OpenInt')
         x[x$OpenInt >= oi_min]$Close
     }
-    100*res[index(res)>dt_start & index(res)<dt_end & !month(index(res))%in%MONTH_CODES$n[MONTH_CODES$code%in%month_letters]]
+    100*res[index(res)>dt_start & index(res)<dt_end & !month(index(res))%in%months_exclude]
+#    100*res[index(res)>dt_start & index(res)<dt_end]
 }
 
 # min_oi = 10; qstart='2008-01-01'; qend='2012-01-01'
-qbasic = function(name, months, weights, min_oi, qstart, qend){
+qbasic = function(name, months, weights, min_oi, qts, dts){
 #    t = rollapply(mmspread(name, months, weights, min_oi, '1990-01-01', '2018-01-01'), FUN=mean, 2)
     t = mmspread(name, months, weights, min_oi, '1990-01-01', '2018-01-01')
-    twnd = t[index(t)>qstart & index(t)<qend]
-    plot(t[index(t)>qend])
+    twnd = t[index(t)>dts[1] & index(t)<dts[2]]
+    plot(t[index(t)>dts[2]])
     #abline(h=0)
-    abline(h=quantile(twnd,0.9))
-    abline(h=quantile(twnd,0.1))
+    abline(h=quantile(twnd,qts[2]))
+    abline(h=quantile(twnd,qts[1]))
 }
+
+# name='LC'; weights=c(1,-2,1); min_oi=1; qts=c(0.3,0.7); an_dts=c('2001-01-01','2008-01-01'); max_date='2018-01-01'
+qspikes = function(name, weights, min_oi, qts, an_dts, max_date){
+    cal = CALENDARS[[name]]
+    res = foreach(i = 1:length(cal),.combine=rbind)%do%{
+        months = c(cal,cal)[i:(i+length(weights)-1)]
+        t = mmspread(name, months, weights, min_oi, an_dts[1], max_date)
+
+        t1 = t[index(t) > an_dts[1] & index(t) < an_dts[2]]
+        t2 = t[index(t) > an_dts[2]]
+
+        print(i)
+        if(length(t1)>100 && length(t2)>100){
+            q1min = as.numeric(quantile(t1, qts[1]))
+            q1max = as.numeric(quantile(t1, qts[2]))
+            t2_years = unique(year(index(t2)))
+            
+            above_cnt = foreach(y=t2_years,.combine=c)%do%sum(t2[year(index(t2))==y] < q1min)
+            below_cnt = foreach(y=t2_years,.combine=c)%do%sum(t2[year(index(t2))==y] > q1max)
+            c(name,months, min(min(above_cnt),min(below_cnt)))
+        }
+    }
+
+    colnames(res) = c('name',paste0('month',1:length(weights)),'flag')
+    as.data.frame(res, stringsAsFactors=FALSE)
+}
+
+s = foreach(name = c('FC','LC','LH'),.combine=rbind)%do%qspikes(name, c(1,-2,1), 5, c(0.3,0.7), c('2008-01-01','2012-01-01'), '2018-01-01')
+names(s)
+s[s$flag>0,]
+
 
 #t = mmspread('FC', c('Q','U','V'), c(1,-2,1), 100, '1990-01-01', '2018-01-01')
 
-plot(mmspread('FC', c('Q','U','V'), c(1,-2,1), 100, '2015-01-01', '2018-01-01'))
-plot(mmspread('FC', c(8,9,10), c(1,-2,1), 100, '2015-01-01', '2018-01-01'))
+plot(mmspread('FC', c('Q','U','V'), c(1,-2,1), 100, '2001-01-01', '2018-01-01'))
+plot(mmspread('LH', c(8,10,12), c(1,-2,1), 5, '2012-01-01', '2018-01-01'))
 # c('F','G','H','J','K','M','N','Q','U','V','X','Z')
 qbasic('FC', c(8,9,10), c(1,-2,1), 100, '2012-01-01', '2015-01-01')
-qbasic('FC', c('Q','U','V'), c(1,-2,1), 100, '2012-01-01', '2015-01-01')
+qbasic('FC', c('Q','U','V'), c(1,-2,1), 100, '2001-01-01', '2015-01-01')
 qbasic('LC', c('J','M','Q'), c(1,-2,1), 2000, '2000-01-01', '2012-01-01')
 
 
-qbasic('FC', CALENDARS[['FC']][1+1:3], c(1,-2,1), 10, '2008-01-01', '2012-01-01')
+qbasic('FC', c('H','J','K'), c(1,-2,1), 100, c(0.3,0.7), c('2001-01-01', '2010-01-01'))
+qbasic('LC', c('G','J','M'), c(1,-2,1), 100, c(0.3,0.7), c('2001-01-01', '2010-01-01'))
+qbasic('FC', CALENDARS[['FC']][0+1:3], c(1,-2,1), 100, c(0.3,0.7), c('2001-01-01', '2008-01-01'))
+qbasic('FC', CALENDARS[['FC']][1+1:3], c(1,-2,1), 100, c(0.3,0.7), c('2001-01-01', '2008-01-01'))
+qbasic('FC', CALENDARS[['FC']][2+1:3], c(1,-2,1), 100, c(0.3,0.7), c('2001-01-01', '2008-01-01'))
+qbasic('FC', CALENDARS[['FC']][3+1:3], c(1,-2,1), 100, c(0.3,0.7), c('2001-01-01', '2008-01-01'))
+qbasic('FC', CALENDARS[['FC']][4+1:3], c(1,-2,1), 100, c(0.3,0.7), c('2001-01-01', '2008-01-01'))
+qbasic('FC', CALENDARS[['FC']][5+1:3], c(1,-2,1), 100, c(0.3,0.7), c('2001-01-01', '2008-01-01'))
+
+qbasic('LC', CALENDARS[['LC']][1+1:3], c(1,-2,1), 7000, c(0.1,0.9), c('1995-01-01', '1998-01-01'))
+
+t = mmspread('FC', CALENDARS[['FC']][1+1:3], c(1,-2,1), 10, '1999-01-01', '2018-01-01')
+d = as.Date(foreach(y = unique(year(index(t))),.combine='rbind')%do%{
+    x = t[year(index(t))==y]
+    index(x)[which.min(x)]
+})
+hist(month(d))
+
+qbasic('FC', CALENDARS[['LH']][1+1:3], c(1,-2,1), 10, '2008-01-01', '2012-01-01')
+
 qbasic('LH', CALENDARS[['LH']][1+1:3], c(1,-2,1), 10, '2008-01-01', '2012-01-01')
 
 
@@ -161,5 +203,37 @@ rngspread('FC', c('Q','U','V'))
 
 
 
+MONTH_CODES$code[CALENDARS[['LC']][1+1:3]]
+n = c('LC/','','')
+r = list()
+for(n in as.vector(unlist(fread('~/FUT/Lots.txt', fill=TRUE)[,1]))){
+    r[[n]] = list()
+    for(m in month_codes$code){
+        x_old = 1
+        for(f in list.files(paste0('~/FUT/',n),recursive=TRUE,full.names=TRUE,pattern=paste0('*',m,'.txt'))){
+            if(nchar(strsplit(f,'/')[[1]][6]) - nchar(n) - 4 == 3)
+            {
+                print(f)
+                x = fread(f)
+                x[,1] = as.Date(unlist(fread(f)[,1]),format='%m/%d/%Y')
+                x = as.xts(x)
+                i = which(diff(index(x))>500)
+                if(length(i) > 0)
+                    x = x[1:i[1]]
+                
+                if(length(x_old) > 1){
+                    x1 = x_old[index(x_old)%in%index(x)]
+                    x2 = x[index(x)%in%index(x_old)]
+                    x12 = if(nrow(x1)==0) x1 else rbind(x1[x1[,6] >= x2[,6]], x2[x2[,6] > x1[,6]])
+                    x_old = rbind(x_old[!index(x_old)%in%index(x)], x12, x[!index(x)%in%index(x_old)])
+                } else {
+                    x_old = x
+                }
+            }
+        }
+
+        r[[n]][[m]] = x_old
+    }
+}
 
 
