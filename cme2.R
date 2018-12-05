3c797caba2f59e61d7d1fe55fca5a545d6704a18
Anton <slepnev@yandex.ru>
2018-12-05 14:30:08 +0300

    aa

--- cme2.R
+++ cme2.R
@@ -4,13 +4,13 @@ library(data.table)
 #library(RCurl)
 #library(lubridate)
 
-#setwd('/home/anton/git/ej/ContractsCSV')
-setwd('/home/aslepnev/git/ej/ContractsCSV')
+setwd('/home/anton/git/ej/ContractsCSV')
+#setwd('/home/aslepnev/git/ej/ContractsCSV')
 comms = fread('../Commodities.csv')
 mon = data.table('num'=1:12, 'month'=c('F','G','H','J','K','M','N','Q','U','V','X','Z'))
 fsizes = foreach(f = list.files('.'), .combine=c)%do%{ file.info(f)$size }
 
-data = rbindlist(foreach(f = list.files('.')[fsizes > 14])%do%{
+data0 = rbindlist(foreach(f = list.files('.')[fsizes > 14])%do%{
     print(f)
     d = fread(f)
     ftoks = strsplit(f,'_')[[1]]
@@ -18,10 +18,37 @@ data = rbindlist(foreach(f = list.files('.')[fsizes > 14])%do%{
     d$month = ftoks[2]
     d$year = as.numeric(ftoks[3])
     d
-})
+})  # save(data0, file='cme_contracts_data.RData') data0=get(load('cme_contracts_data.RData'))
 
-contracts = mon[unique(data[, .(commodity, month, year)]), on='month'][order(year, num), ][, id:=1:.N][, idloc:=1:nrow(.SD), by='commodity']
-data = contracts[, .(commodity, month, year, id, idloc)][data, on=.(commodity, month, year)]
+contracts = mon[unique(data0[, .(commodity, month, year)]), on='month'][order(year, num), ][, id:=1:.N][, idloc:=1:nrow(.SD), by='commodity'][, num:=NULL]
+data = contracts[, .(commodity, month, year, id, idloc)][data0, on=.(commodity, month, year)]
+
+# comdtys=c('Feeder Cattle', 'Lean Hog'); lags=1:2; min_year=1990
+build_nplets = function(comdtys, data0, lags, min_year){
+    data = data0[commodity%in%comdtys & year>=min_year, ]
+    contracts = mon[unique(data[, .(commodity, month, year)]), on='month'][order(year, num), ][, id:=1:.N][, idloc:=1:nrow(.SD), by='commodity'][, num:=NULL]
+    data = contracts[, .(commodity, month, year, id, idloc)][data, on=.(commodity, month, year)]
+
+    icols = c('month', 'year', 'id', 'idloc', 'Close', 'OI')
+    data1 = data[, .SD, .SDcols=c(icols, 'commodity', 'Date')]
+    colnames(data1) = c(paste0(icols,0), 'commodity', 'Date')
+    for(i in lags){
+        idloci = paste0('idloc', i)
+        data1[, eval(idloci):=idloc0+i]
+        datai = data[, .SD, .SDcols=c(icols, 'commodity', 'Date')]
+        colnames(datai) = c(paste0(icols,i), 'commodity', 'Date')
+        data1 = datai[data1, on=c('commodity', 'Date', idloci)][!is.na(get(paste0('Close', i))), ]
+    }
+    return(data1)
+}
+
+# nplets = data1; weights=c(1,-2,1)
+calc_spreads = function(nplets, weights){
+    res = nplets
+    res$spread = foreach(i=1:length(weights),.combine='+')%do%{ weights[i]*res[, get(paste0('Close', i-1))] }
+    res$year = eval(parse(text=paste0('pmin(', paste(paste0('res$year', 1:length(weights)-1), collapse=','),')')))
+    
+}
 
 
 
@@ -29,8 +56,7 @@ data = contracts[, .(commodity, month, year, id, idloc)][data, on=.(commodity, m
 
 
 
-
-
+s
 
 
 
