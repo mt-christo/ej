275a5ee4ebcc80c1c42f3452692517d7213f62b5
anton <aslepnev@kart-tree.ru>
2018-12-21 20:12:45 +0300

    aa

--- cmefunc.R
+++ cmefunc.R
@@ -19,9 +19,30 @@ spread_ival_tpdd = function(data, dt_start, dt_end){
     return(s2)
 }
 
-# comdty='Feeder Cattle'; spread_months='F-H'; dt_start='2018-11-09'; dt_end='2019-01-04'
-get_spread_wnd <- function(data, comdty, spread_months, dt_start, dt_end){
-    res = s[commodity==comdty & months==spread_months, ]
-    res = spread_ival(res, dt_start, dt_end)[, .(zerodate, date, year, spread)]
+# comdty='Feeder Cattle'; spread_months='U-V-X'; dt_start='2014-03-09'; wnd=15
+get_spread_wnd_fwd <- function(comdty, spread_months, dt_start, wnd){
+    chgfun = function(x) { tail(x,1) - x[1] }
+    s = Spreads[commodity==comdty & months==spread_months, ]
+
+    zero_year = 1901 + year(dt_start) - year(s[expiry > dt_start, min(expiry)])
+    zero_start = as.Date(ISOdate(zero_year, month(dt_start), day(dt_start)))
+    s = s[, zero_date:=as.Date(ISOdate(1901+year(date)-year(expiry), month(date), day(date)))][!is.na(zero_date), ]
+    s = s[CJ(zero_date = unique(s$zero_date), year=unique(s$year)), on=.(zero_date, year)][order(zero_date), ]
+    s = s[, ':='(spread = na.locf(spread, na.rm=FALSE)
+                ,ma5=rollapply(spread, 5, FUN=mean, fill=NA, align='right')
+                ,ma10=rollapply(spread, 10, FUN=mean, fill=NA, align='right')
+                ), by=year]
+    s = s[, ':='(ma5chg3 = rollapply(ma5, 3, FUN=chgfun, fill=NA, align='right')
+                ,ma5chg5 = rollapply(ma5, 5, FUN=chgfun, fill=NA, align='right')
+                ,ma5chg7 = rollapply(ma5, 7, FUN=chgfun, fill=NA, align='right')
+                ,ma10chg3 = rollapply(ma10, 3, FUN=chgfun, fill=NA, align='right')
+                ,ma10chg5 = rollapply(ma10, 5, FUN=chgfun, fill=NA, align='right')
+                ,ma10chg7 = rollapply(ma10, 7, FUN=chgfun, fill=NA, align='right')
+                ), by=year]
+    res = s[zero_date>=zero_start & zero_date<=zero_start+wnd, ][, !colnames(s)%in%c('commodity','date','months','expiry'), with=FALSE]
     return(res)
+}
+
+get_spread_wnd <- function(comdty, spread_months, dt_start, dt_end){
+    return(get_spread_wnd_fwd(comdty, spread_months, dt_start, dt_end-dt_start))
 }
