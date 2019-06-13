1532c51af8c6dff50b2b294df5ca7751ee6b97c4
Anton <root@novo-x.info>
2019-06-14 01:20:13 +0300

    aa

--- novo_static_prep.R
+++ novo_static_prep.R
@@ -79,9 +79,19 @@ TAG_FILTERS = rbindlist(foreach(t = TAG_FILTERS)%do%{
 # -- UNI FILTERS
 
 
+
 # FACTOR MODEL
 
-BASIC_MODEL = c('pe', 'div', 'peg', 'de', 'pb')
+METRIC_NORM_OVER_UNI = c('pe', 'div')
+METRIC_NORM_OVER_SECTOR = c('logmcap', 'sharpe')
+
+LOADING_NORM_OVER_UNI = c('value', 'size')
+LOADING_NORM_OVER_SECTOR = c('momentum', 'volatility')
+
+BASIC_MODEL = list(value = list(c('pe', 'div'), c(1, 1))
+                   volatility = list(c('sigma20', 'sigma40'), c(1, 1))
+                   size = list('logmcap', 1)
+                   momentum = list(c('pm12m1m', 'sharpe'), c(1, 1)))
 
 
 # -- FACTOR MODEL
